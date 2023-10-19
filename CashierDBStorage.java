9de8f044035ff55c69f313992f8f5a055d172e88
All-Missing <phk6927@autuni.ac.nz>
2023-10-20 08:36:26 +1300

    modify CashierDBStorage

--- CashierDBStorage.java
+++ CashierDBStorage.java
@@ -34,31 +34,24 @@ public class CashierDBStorage {
         
         try {
             //connect and initialize db
-            statement = conn.createStatement();
-        
+            statement = conn.createStatement();        
             String tableName = "PRODUCT";             
             checkExistedTable(tableName); //Check if table exisits. if not, manually create it
-            
-            String sqlTable = "CREATE TABLE " + tableName + " (item_id VARCHAR(10), "
-                    + "item VARCHAR(50), item_price DOUBLE, category VARCHAR(20))";                    
-            statement.executeUpdate(sqlTable);
-            System.out.println("Table " + tableName +" created");
+            this.statement.addBatch("CREATE TABLE " + tableName + " (item_id VARCHAR(10) PRIMARY KEY, "
+                    + "item VARCHAR(50), item_price DOUBLE, category VARCHAR(20))");
             
             //Retrieve product records value and insert data into sql table
             HashMap<String, Product> product_records = productList.getProduct_records();                        
-            for (Map.Entry<String, Product> entry: product_records.entrySet()) {
-                
+            for (Map.Entry<String, Product> entry: product_records.entrySet()) {                
                 String item_id = entry.getKey();
                 Product product = entry.getValue();
-                
-                String insertQuery = "INSERT INTO " + tableName + " VALUES ('"
+                this.statement.addBatch("INSERT INTO " + tableName + " VALUES ('"
                         + item_id + "', '"
                         + product.getItem() + "', "
                         + product.getItemPrice() + ", '"
-                        + product.getCategory() + "')";
-                         
-                statement.executeUpdate(insertQuery);
-            }                        
+                        + product.getCategory() + "')");                                                         
+            }
+            this.statement.executeBatch();
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }          
@@ -71,18 +64,17 @@ public class CashierDBStorage {
             //connect and initialize db
             this.statement = conn.createStatement();  
             this.checkExistedTable("STAFF");//Check if table exisits. if not, manually create it            
-            this.statement.addBatch("CREATE TABLE STAFF (staffID VARCHAR(10), staff_name VARCHAR(20))");
+            this.statement.addBatch("CREATE TABLE STAFF (staff_id VARCHAR(10) PRIMARY KEY, staff_name VARCHAR(20))");
             this.statement.executeBatch();
+            
 //            Retrieve product records value and insert data into sql table
             HashMap<String, String> staff_records = staffList.getStaff_list();
             for (Map.Entry<String, String> entry: staff_records.entrySet()) {                
                 String staff_id = entry.getKey();
                 String staff_name = entry.getValue();               
-                this.statement.addBatch("INSERT INTO STAFF VALUES ('"
-                        + staff_id + "', '"                       
-                        + staff_name + "')");                         
-                this.statement.executeBatch();
+                this.statement.addBatch("INSERT INTO STAFF VALUES ('"+ staff_id+"', '"+ staff_name + "')");                         
             }                        
+            this.statement.executeBatch();
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }          
@@ -94,21 +86,33 @@ public class CashierDBStorage {
         try {
             this.statement = conn.createStatement();
             this.checkExistedTable("BILL_ORDER");
-            this.statement.addBatch("CREATE TABLE BILL_ORDER (order_id INT, bill DOUBLE)");            
-            
-            //Retrieved data.
+            this.statement.addBatch("CREATE TABLE BILL_ORDER (shift_id INT, staff_id VARCHAR(10),"
+                    + " order_id INT, bill DOUBLE)");                       
+                        
             BufferedReader br;            
-            try {                                
+            try {   //Retrieved data.                                
                 br = new BufferedReader(new FileReader("./file_records/BillOrder_Records.txt"));
                 String line ="";
+                String[] lineParts;
+                Integer shift_id = 0;
+                Integer order_id = 0;
+                Double bill = 0.0;
+                String staff_id = null;
+                
                 while ((line = br.readLine()) != null) {
-                    if (line.startsWith("OrderID: ")) {
-                        String[] lineParts = line.split(" ");
-                        Integer order_id = Integer.parseInt(lineParts[1]);
-                        Double bill = Double.parseDouble(lineParts[4]);
-                        this.statement.addBatch("INSERT INTO BILL_ORDER VALUES("+ order_id+", " +bill+ ")");
-                        this.statement.executeBatch();
-                    }                        
+                    if (line.startsWith("---ShiftID: ")) {
+                        lineParts = line.split(" ");
+                        shift_id = Integer.parseInt(lineParts[1]);
+                        staff_id = lineParts[3];
+                    }   else if (line.startsWith("OrderID: ")) {
+                        lineParts = line.split(" ");
+                        order_id = Integer.parseInt(lineParts[1]);
+                        bill = Double.parseDouble(lineParts[4]);
+                        
+                        //Insert data into Bill_Order table
+                        this.statement.addBatch("INSERT INTO BILL_ORDER VALUES ("
+                                + shift_id +",'"+staff_id + "', " + order_id + ", " + bill + ")");                        
+                    }
                 }
                 br.close();
             } catch (IOException e) {
@@ -122,6 +126,37 @@ public class CashierDBStorage {
     
     public void createBalanceDB() {
         
+        try {
+            this.statement = conn.createStatement();
+            this.checkExistedTable("BALANCE");
+            this.statement.addBatch("CREATE TABLE BALANCE (shift_id INT PRIMARY KEY, staff_id VARCHAR(10), "
+                    + "staff_name VARCHAR(20), total_balance DOUBLE)");            
+            RetrieveCashierDB retrieveDB = new RetrieveCashierDB();
+            
+            BufferedReader br;
+            try {
+                br = new BufferedReader(new FileReader("./file_records/BillOrder_Records.txt"));
+                String line = "";
+                while ((line = br.readLine()) != null) {
+                    if (line.startsWith("---ShiftID: ")) {
+                      String[] lineParts = line.split(" ");
+                      Integer shift_id = Integer.parseInt(lineParts[1]);
+                      String staff_id = lineParts[3];
+                      String staff_name = lineParts[5];
+                      Double totalBalance = retrieveDB.getBillOrderPerfShift(shift_id);
+                      
+                      //Insert data into Balance table
+                      this.statement.addBatch("INSERT INTO BALANCE VALUES (" + shift_id + ", '"
+                              + staff_id + "', '"+ staff_name+"', "+totalBalance+")");
+                    }                                  
+                }
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }                                  
+            this.statement.executeBatch();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
         
     }
     
